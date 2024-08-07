5dcebc6a1a38b25ae8c21e3d4ec0b22b7d4f0b8c
NguyenDucHieu <nguyenduchieu647484@gmail.com>
2024-08-07 21:36:01 +0700

    first commit

---
+++ REACT-WEBACK/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react' 
+import ReactDOM from 'react-dom' 
+import App from './App'
+// Tạo component App
+const container = ReactDOM.createRoot(document.getElementById('root'))
+
+container.render(
+    <>
+        <App />
+    </>
+)
+
---
+++ REACT-WEBACK/webpack.config.js
@@ -0,0 +1,53 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  entry: "./src/index.js", // Dẫn tới file index.js ta đã tạo
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    publicPath: '/',
+    assetModuleFilename: 'images/[name][ext]',
+    clean: true
+},
+  devServer: {
+    compress: true,
+    port: 8080,
+    historyApiFallback: true,
+},
+  module: {
+    rules: [
+      {
+        test: /\.css$/, // Sử dụng style-loader, css-loader cho file .css
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+          }
+        }
+      },
+      {
+        test: /\.html$/i,
+        use: ['html-loader'],
+      },
+      
+    ]
+    
+  },
+  // Chứa các plugins sẽ cài đặt trong tương lai
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'public', 'index.html')
+    }),
+    // new MiniCssExtractPlugin({
+    //     filename: '[name].css',
+    //     chunkFilename: '[id].css',
+    // })
+  ]
+};
