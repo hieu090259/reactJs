44799e26920d57518d61afb4f1a0d3872b3012fb
NguyenDucHieu <nguyenduchieu647484@gmail.com>
2024-08-08 22:20:24 +0700

    REACT

--- REACT-WEBACK/src/App.js
+++ REACT-WEBACK/src/App.js
@@ -1,7 +1,11 @@
 import React from 'react' 
+import HeaderComponent from './components/headerComponent/headerComponent.jsx'
 
 export default function App(props) {
     return (
-        <h1>hello world</h1>
+        <>
+            <h1>hello world</h1>
+            <HeaderComponent/>
+        </>       
     )
 }
---
+++ REACT-WEBACK/src/components/headerComponent/headerComponent.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+export default HeaderComponent = (props) => {
+    
+    return (
+      <div className="container ">
+        <div className="row align-items-center header-middle">
+            <button className="btn btn-primary"></button>
+            <h1>ashd</h1>
+        </div>
+      </div>
+    )
+  }
+  
+
